57c8b0980d2594c27029f36243803bc65ed665bc
cobb <stevenapollo@163.com>
2026-07-25 17:31:26 +0800

    add serial_read function

--- serial_transport.py
+++ serial_transport.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+from crc import check_modbus_crc16
 
 def serial_get(serial_name: str, baudrate:int, timeout:int) -> serial.Serial:
     ports = []
@@ -31,3 +32,15 @@ def serial_transmit(port: serial.Serial, data: bytes):
     if transmit_len != len(data):
         raise RuntimeError(f" transmit is not complete, actually transmit number is {transmit_len} ")
 
+def serial_receive(port: serial.Serial, data:bytearray) -> bool:
+    port.open()
+    read_bytes = port.read()                                # max read block time = timeout
+    port.close()
+    
+    if check_modbus_crc16(bytearray(read_bytes)):
+        data = bytearray(read_bytes)
+        print(f" INFO: serial read data {data} ")
+        return True
+    else:
+        print(f" ABNORMAL: serial data crc error {read_bytes} ")
+        return False
