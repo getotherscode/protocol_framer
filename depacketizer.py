9d15fa2bc011830c2f9ce07f909c9499ba34125c
cobb <stevenapollo@163.com>
2026-07-25 17:33:43 +0800

    split depacketizer function into get file, parse file, pack

--- depacketizer.py
+++ depacketizer.py
@@ -1,21 +1,30 @@
-import glob       # for fuzzy file name search
+import glob                                                       # for fuzzy file name search
+import os
 
-def depacketizer(pack_size:int):
+def get_bin_file_path() -> list[str]:
     bin_files = glob.glob("*.bin")
     if not bin_files:
         raise FileNotFoundError("do not find any .bin file !!!")  # raise can terminate the execution
     if len(bin_files) > 1:
         raise RuntimeError(".bin file is not only one !!!")
+    
+    print(f"find the target bin file {bin_files[0]}")
+    return bin_files[0]
 
-    bin_dir = bin_files[0]
-    print(f"find the target bin file {bin_dir}")
-    with open(bin_dir, "rb") as f:                                # with as is context manager, close file automatically, "rb" read only binary
+def parse_bin_file_size(path: list[str]) -> int:
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"{path} do not exist !!!")
+    bin_size = os.path.getsize(path)
 
-        pack_idx = 1
-        while True:
-            pack = f.read(pack_size)                              # if left bytes is less than 1024, will return all left bytes
+    print(f"{path} file size: {bin_size}")
+    return bin_size
+    
+def depacketizer(single_pack_size:int, path: list[str], start_addr: int):
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"{path} do not exist !!!")
 
-            if not pack:
-                break
-            yield (pack_idx, pack)
-            pack_idx += 1
+    with open(path, "rb") as f:                                   # with as is context manager, close file automatically, "rb" read only binary
+            f.seek(start_addr)
+            pack = f.read(single_pack_size)                       # if left bytes is less than 1024, will return all left bytes
+            if not pack: raise RuntimeError(f".bin read from {start_addr} is failed  !!!")
+            return pack
