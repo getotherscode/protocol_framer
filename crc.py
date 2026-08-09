6b0f2e3d33f9677f8a6dceaa4fe399382666d923
cobb <stevenapollo@163.com>
2026-08-09 19:27:44 +0800

    modbus crc calculation and check

--- crc.py
+++ crc.py
@@ -8,8 +8,26 @@ def get_modbus_crc16(frame: bytearray) -> bytes:
 def check_modbus_crc16(frame: bytes) -> bool:
     frame_len = len(frame)
     if frame_len > 2:
-        crc = (frame[frame_len - 1] << 8) + frame[frame_len - 2]
-        crc_check = get_modbus_crc16(bytearray(frame))
+        crc = frame[frame_len - 1] + (frame[frame_len - 2] << 8)
+        check_slice = frame[0:len(frame)-2]
+        # print(f"check slice: {check_slice.hex(' ')}")
+
+        crc_bytes = get_modbus_crc16(bytearray(check_slice))
+        crc_check = crc_bytes[1] + (crc_bytes[0] << 8)
+        # print(f"get crc: {crc}, check crc: {crc_check}")
+        
         if crc == crc_check:
             return True
     return False
+
+def modbus_crc16_update(data: bytearray, crc: int) -> bytes:
+    for byte in data:
+        crc ^= byte
+
+        for _ in range(8):
+            if crc & 0x0001:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc >>= 1
+
+    return crc & 0xFFFF
